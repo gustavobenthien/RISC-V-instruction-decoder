930cf5c889f9674c7171ff12ad958a48a42d1c00
unknown <gbenthien669@gmail.com>
2026-09-22 20:09:13 -0300

    feat: machine instruction to reconstructed assembly instruction type r

--- decoder.cpp
+++ decoder.cpp
@@ -2,32 +2,17 @@
 #include <iostream>
 #include "instruction-type.hpp"
 #include "instruction-mnemonic.hpp"
-#include "instruction-type.hpp"
+#include "instruction-recomposition.hpp"
 #include "file-interpreter.hpp"
 
 using namespace std;
 
-void detectType(string binary, string values[]) {
-
-  // TIPO R
-  if(opcode(binary) == "0110011") {
-
-    typeR(binary, values);
-
-    cout << "format: R" << endl; 
-    cout << "funct7: " << binaryIntConverter(values[0]) << endl;
-    cout << "rs2: " << binaryIntConverter(values[1]) << endl;
-    cout << "rs1: " << binaryIntConverter(values[2]) << endl;
-    cout << "funct3: " << binaryIntConverter(values[3]) << endl;
-    cout << "rd: " << binaryIntConverter(values[4]) << endl;
-  }
-}
 
 int main() {
   string values[5];
 
-  detectType(lineReader(), values);
-  detectType(lineReader(), values);
+  cout << instructionReconstruction(lineReader(), values) << endl;  
+  cout << instructionReconstruction(lineReader(), values);  
 
   return 0;
 }
--- instruction-recomposition.hpp
+++ instruction-recomposition.hpp
@@ -1,5 +1,42 @@
+#pragma once
 #include <iostream>
+#include "instruction-type.hpp"
+#include "instruction-mnemonic.hpp"
+#include "file-interpreter.hpp"
+
 using namespace std;
 
-string instructionReconstruction(string binary, char type) {
+string instructionReconstruction(string binary, string values[]) {
+
+  // TIPO R
+  if(opcode(binary) == "0110011") {
+
+    typeR(binary, values);
+
+    return mnemonicTypeR(values[3], values[0]) +
+           " x" + to_string(binaryIntConverter(values[4])) +
+           ", x" + to_string(binaryIntConverter(values[2])) +
+           ", x" + to_string(binaryIntConverter(values[1]));
+
+  }
+  // TIPO I ALU
+  else if(opcode(binary) == "0010011") {
+
+  }
+  // TIPO I LOAD
+  else if(opcode(binary) == "0000011") {
+
+  }
+  // TIPO S
+  else if(opcode(binary) == "0100011") {
+
+  }
+  // TIPO B
+  else if(opcode(binary) == "1100011") {
+
+  }
+  // TIPO J
+  else if(opcode(binary) == "1101111") {
+
+  }
 }
