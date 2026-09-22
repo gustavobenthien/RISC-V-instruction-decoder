83a6cb79a3f4a5e4d4beeb84606aa5e9bfd9bc81
Gustavo Benthien <gbenthien669@gmail.com>
2026-09-22 00:34:34 -0300

    fix: faulty order of operations in binary converter function

--- decoder.cpp
+++ decoder.cpp
@@ -5,6 +5,30 @@ using namespace std;
 int pc = 0;
 
 
+string hexaToBinary(string hexadecimal) {
+
+  string table[16] = {
+    "0000", "0001", "0010", "0011",
+    "0100", "0101", "0110", "0111",
+    "1000", "1001", "1010", "1011",
+    "1100", "1101", "1110", "1111"
+  };
+
+  string binary = "";
+
+  for(char c : hexadecimal.substr(2, 8)) {
+
+    if(c >= 48 && c <= 57) {
+      binary += table[c - 48];
+    }
+    else if(c >= 65 && c <= 70) {
+      binary += table[(c - 65) + 10];
+    }
+  }
+
+  return binary;
+}
+
 string lineReader() {
 
   /*
@@ -30,31 +54,6 @@ string lineReader() {
   return "ERROR";
 }
 
-string hexaToBinary(string hexadecimal) {
-
-  string table[16] = {
-    "0000", "0001", "0010", "0011",
-    "0100", "0101", "0110", "0111",
-    "1000", "1001", "1010", "1011",
-    "1100", "1101", "1110", "1111"
-  };
-
-  string binary;
-
-  for(char c : hexadecimal.substr(2, 8)) {
-
-    if(c >= 48 || c <= 57) {
-      binary += table[c - 48];
-    }
-
-    if(c >= 65 || c <= 70) {
-      binary += table[(c - 65) + 10];
-    }
-  }
-
-  return binary;
-}
-
 void divideBinary(string binary, string list[]) {
 
   /*
@@ -73,11 +72,15 @@ void divideBinary(string binary, string list[]) {
 }
 
 int main() {
-  string aux;
+  string aux[5];
+  string value = hexaToBinary("0x00500413");
 
-  for(int i = 0; i < 6; i++) {
-    cout << "Next line: ";
-    cin >> aux;
-    cout << lineReader() << endl;
+  cout << value << endl;
+
+  divideBinary(value, aux);
+
+  for(int i = 0; i < 5; i++) {
+    cout << aux[i] << endl;
   }
+ 
 }
